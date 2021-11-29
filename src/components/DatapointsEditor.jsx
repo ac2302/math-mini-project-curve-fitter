68098b831a779f4f3d69ab9c79d313588cecbee1
Aryan Chavan <aryan@aryansdomain.com>
2021-11-29 23:49:34 +0530

    add points selector

---
+++ src/components/DatapointsEditor.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+
+function DatapointsEditor({ datapoints, setDatapoints }) {
+	let [latest, setLatest] = useState(0);
+
+	return (
+		<div className="datapoints-editor">
+			{datapoints.map((datapoint) => (
+				<div key={datapoint.id}>
+					{/* {datapoint.x} {datapoint.y} {datapoint.id} */}
+					<DatapointEditor
+						datapoints={datapoints}
+						setDatapoints={setDatapoints}
+						id={datapoint.id}
+						key={datapoint.id}
+					/>
+				</div>
+			))}
+			<button
+				onClick={() => {
+					setDatapoints([
+						...datapoints,
+						{ x: latest, y: 2 * latest, id: latest },
+					]);
+					setLatest(latest + 1);
+				}}
+			>
+				new item
+			</button>
+		</div>
+	);
+}
+
+function DatapointEditor({ datapoints, setDatapoints, id }) {
+	let coords = datapoints.filter((dp) => dp.id == id)[0];
+
+	return (
+		<div>
+			<code>x = </code>
+			<input
+				type="number"
+				defaultValue={coords.x}
+				onChange={(e) => {
+					setDatapoints((prevState) =>
+						prevState.map((entry) => {
+							if (entry.id === id) entry.x = Number(e.target.value);
+							return entry;
+						})
+					);
+				}}
+			/>
+			<code>y = </code>
+			<input
+				type="number"
+				defaultValue={coords.y}
+				onChange={(e) => {
+					setDatapoints((prevState) =>
+						prevState.map((entry) => {
+							if (entry.id === id) entry.y = Number(e.target.value);
+							return entry;
+						})
+					);
+				}}
+			/>
+			<button
+				onClick={() => {
+					setDatapoints((prevState) => {
+						return prevState.filter((entry) => entry.id !== id);
+					});
+				}}
+			>
+				remove
+			</button>
+		</div>
+	);
+}
+
+export default DatapointsEditor;
